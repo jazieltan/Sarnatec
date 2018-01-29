d3a6a65f2bf2ab2c063eb417278eecfa4a7d7849
jazieltan <jazieltan@gmail.com>
2018-01-29 15:33:47 +0800

    removed script from app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-// $(document).foundation();
-
-// var $grid = $('.grid').imagesLoaded(function () {
-    // init Masonry after all images have loaded
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true
-    });
-// });  
+$(document).foundation();
