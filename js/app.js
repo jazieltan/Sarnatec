47b2ff830f2bee17d26e69c9dc39ddd7e1b468fc
jazieltan <jazieltan@gmail.com>
2018-01-29 15:11:41 +0800

    Finished template build
    Build template with images
    all pages done.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+// var $grid = $('.grid').imagesLoaded(function () {
+    // init Masonry after all images have loaded
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+// });
