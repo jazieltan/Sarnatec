8860040ac86c55b7f6d7db14f1413c5da5fdbc35
jazieltan <jazieltan@gmail.com>
2018-02-03 17:55:47 +0800

    Converted site to jekyll
    site conversion to jekyll

---
+++ jekyll/_site/js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation(
+);
+var $grid = $('.grid').imagesLoaded( function() {
+  $grid.masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+  });
+});
